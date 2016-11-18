3a5d825af13abfb25897c9428ba1be72a2147c93
Nick Block <nicholasblock78@gmail.com>
2016-11-18 17:19:00 -0600

    able to log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new
+	end
 
-  def create
-  	user = User.find_by(email: params[:email])
-  	user.authenticate(params[:password])
-  	redirect_to '/'
-  end
+	def create
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect_to root_path
+		else
+			flash.now[:error] = "There was a problem authenticating."
+			render action: 'new'
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		flash.now[:logout_success] = "You have successfully logged out."
+		redirect_to '/'
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  get 'sessions/create'
+  post 'sessions/create'
+
+  delete 'sessions/destroy'
+
 
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
