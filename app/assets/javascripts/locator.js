7686687e69bcc9ed3acaaabd628b773a87be393f
Nick Block <nicholasblock78@gmail.com>
2016-11-19 13:09:26 -0600

    walking away from the computer

--- app/assets/javascripts/locator.js
+++ app/assets/javascripts/locator.js
@@ -1,91 +1,88 @@
-/////////////API Function Interface////////////////////
+///////////// Google Maps API Interface ////////////////////
 var marketId = [];
-  var allLatlng = [];
-  var allMarkers = [];
-  var marketName = [];
-  var infowindow = null;
-  var pos;
-  var userCords;
-  var tempMarkerHolder = [];
+var allLatlng = [];
+var allMarkers = [];
+var marketName = [];
+var infowindow = null;
+var pos;
+var userCords;
+var tempMarkerHolder = [];
 
 
-  var mapOptions = {
-    zoom: 5,
-    center: new google.maps.LatLng(37.09024, -100.712891),
-    panControl: false,
-    panControlOptions: {
-      position: google.maps.ControlPosition.BOTTOM_LEFT
-    },
-    zoomControl: true,
-    zoomControlOPtions: {
-      style: google.maps.ZoomControlStyle.LARGE,
-      position: google.maps.ControlPosition.RIGHT_CENTER
-    },
-    scaleControl: false
-  }; 
+var mapOptions = {
+  zoom: 5,
+  center: new google.maps.LatLng(37.09024, -100.712891),
+  panControl: false,
+  panControlOptions: {
+    position: google.maps.ControlPosition.BOTTOM_LEFT
+  },
+  zoomControl: true,
+  zoomControlOPtions: {
+    style: google.maps.ZoomControlStyle.LARGE,
+    position: google.maps.ControlPosition.RIGHT_CENTER
+  },
+  scaleControl: false
+}; 
 
-  infowindow = new google.maps.InfoWindow({
-    content: 'hold on there...'
-  });
+infowindow = new google.maps.InfoWindow({
+  content: 'hold on there...'
+});
 
+var googleLink = response['GoogleLink']
+var latLng = decodeURIComponent(googleLink.substring(googleLink.indexOf("=")+1, googleLink.lastIndexOf("(")));
+var split = latLng.split(',')
+var lat = split[0];
+var lng = split[1];
 
+///////////// USDA Farmer's Market Database API Interface ////////////////////
+var detailed = function(id) {
+  $.ajax({
+    type: "GET",
+    contentType: "application/json; charset=utf-8",
+    url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
+    dataType: 'jsonp'})
 
-
-
-
-          // geocodeAddress(geocoder, map);
-          console.log('goodbye there')
-
-
-          var detailed = function(id) {
-            $.ajax({
-              type: "GET",
-              contentType: "application/json; charset=utf-8",
-              url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=" + id,
-              dataType: 'jsonp'})
-
-            .done(function(response) {
-    // console.log(response)
-    console.log(response.marketdetails)
+  .done(function(response) {
+    console.log(response.marketdetails.GoogleLink)
     $('#locator-results').append(response.marketdetails.Address)
     console.log('hellloooooooooo')
     console.log(response.marketdetails.Address)
 
   })
 
-          }
+}
 
-          var zipcode = function(zipcode) {
-           $.ajax({
-            type: "GET",
-            contentType: "application/json; charset=utf-8",
-            url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zipcode,
-            dataType: 'jsonp'})
+var zipcode = function(zipcode) {
+ $.ajax({
+  type: "GET",
+  contentType: "application/json; charset=utf-8",
+  url: "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + zipcode,
+  dataType: 'jsonp'})
 
-           .done(function(response) {
-            console.log(response)
-            console.log(response.results[0].id)
-            $('#locator-results').append(response.results[0].marketname)
+ .done(function(response) {
+  console.log(response)
+  console.log(response.results[0].id)
+  $('#locator-results').append(response.results[0].marketname)
 
-            var marketId = response.results[0].id
-            var marketname = response.results[0].marketname
-            var nameData = $.param({"marketname": marketname})
+  var marketId = response.results[0].id
+  var marketname = response.results[0].marketname
+  var nameData = $.param({"marketname": marketname})
 
-            detailed(marketId.toString())
+  detailed(marketId.toString())
 
-            $.ajax({
-             method: 'post',
-             url: '/marketname',
-             data: nameData,
-           })
-          })
-         }
+  $.ajax({
+   method: 'post',
+   url: '/marketname',
+   data: nameData,
+ })
+})
+}
 
 
 ////////////PAGE LOADED////////////////
-//Want it to show a fetching results screen
-//then pivot to the google maps of the directions
 $(document).ready(function() {
+  mapOptions['center'] = new google.maps.LatLng(40.09024, -105.712891);
+  mapOptions['zoom'] = 2;
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
   $('#farm-form').on('submit', function(event) {
@@ -105,7 +102,7 @@ $(document).ready(function() {
   });
 
 
-  })
+})
 
 
 
